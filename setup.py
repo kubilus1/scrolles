25642f511f5f8f8d2ed45890a8f6c7de7c5e6444
kubilus1 <mattkubilus@gmail.com>
2017-06-30 13:30:43 -0400

    Add basic setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="ScrollES",
+    version="0.1",
+    description="Tail ELK logs through ElasticSearch",
+    author="Matt Kubilus",
+    url="https://github.com/kubilus1/scrolles",
+    scripts=["scrolles.py"]
+)
